9a45d34afa98a05160f1f5a1fed5ad80a9875d63
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-01-21 20:09:17 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shestmintsev-kirill/my-profil.git master:gh-pages
+git push -f git@github.com:shestmintsev-kirill/my-profile.git master:gh-pages
 
 cd -
