8447d35fd2a1b40f2ef7dc15a0a67b121bdb12df
shestmintsev-kirill <1kirillmihail1@gmail.com>
2021-07-19 10:59:04 +0300

    add head transition navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav @scroll.passive="scroll" class="nav">
+  <nav @scroll="handleScroll()" :class="{ scroll: isScroll }" class="nav">
     <ul v-scroll-spy-active v-scroll-spy-link>
       <li v-for="link in links" :key="link">
         <a>{{ $t(link) }}</a>
@@ -18,8 +18,31 @@ export default {
       "Navbar.skills",
       "Navbar.portfolio",
       "Navbar.contact"
-    ]
-  })
+    ],
+    isScroll: false,
+    scrollPosition: 0
+  }),
+
+  methods: {
+    handleScroll() {
+      if (
+        this.scrollPosition < window.pageYOffset &&
+        window.pageYOffset > 250 &&
+        document.documentElement.clientWidth > 565
+      ) {
+        this.isScroll = true;
+      } else {
+        this.isScroll = false;
+      }
+      this.scrollPosition = window.pageYOffset;
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 };
 </script>
 
@@ -38,6 +61,7 @@ export default {
   align-items: center;
   border-bottom: 1px solid rgba(122, 122, 122, 0.3);
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  transition: 0.3s;
 
   ul {
     width: 100%;
@@ -50,6 +74,18 @@ export default {
     }
   }
 }
+
+.scroll {
+  height: 55px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  background-color: #fff;
+
+  .active {
+    background: rgb(224, 224, 224);
+  }
+}
+
 @media (max-width: $screen-xs-max) {
   .nav {
     display: none;
